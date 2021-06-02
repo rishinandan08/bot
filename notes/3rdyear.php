7d5329fe02e634c583ff71c208f4370f5f97c203
rishinandan08 <65062233+rishinandan08@users.noreply.github.com>
2021-06-02 11:46:54 +0530

    update

--- notes/3rdyear.php
+++ notes/3rdyear.php
@@ -12,6 +12,9 @@ include "navbar.php";
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>NOTES | 3rd year</title>
 </head>
 <style>
